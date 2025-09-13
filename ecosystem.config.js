d4ef6e5033f137ea7087d5aa997d1196a6538d08
basubhatt1996 <basubhatt27@gmail.com>
2025-09-13 12:47:38 +0530

    updating ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "my-app",
     script: "src/app.js",   // or "app.js" / "server.js" depending on your entry file
-    cwd: "/opt/app",        // explicitly run from /opt/app
+    cwd: "/opt/app/actions-runner/_work/gitActions/gitActions",        // explicitly run from /opt/app
     instances: 1,
     autorestart: true,
     watch: false,
