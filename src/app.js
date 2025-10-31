dff965af75533bc17c991fa827e554fa53e60041
basubhatt1996 <basubhatt27@gmail.com>
2025-10-31 13:10:17 +0530

    new

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World Hi!" });
+  res.json({ message: "Hi welcome to mapi" });
 });
 
 app.get("/sum", (req, res) => {
